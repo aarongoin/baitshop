d142ef61c09d8cd02352b16ceff30a4979e7e35b
Aaron Goin <aarondgoin@icloud.com>
2020-05-14 21:55:48 -0700

    first test

---
+++ src/tests/Hook.test.tsx
@@ -0,0 +1,26 @@
+// import * as React from "react";
+// import { render } from "@testing-library/react";
+import { Hook } from "../baitshop";
+
+describe("Hook class", () => {
+  test("constructor sets props, state, and bait", () => {
+    class HookA extends Hook<{ foo: string }, { hello: string }, { testAction: () => string }> {
+      initialState() {
+        return { hello: "world" };
+      }
+      getActions() {
+        return { testAction: () => "works!" }
+      }
+    }
+    const props = { foo: "bar" };
+    const instance = new HookA(props);
+
+    expect(instance.props).toBe(props);
+
+    expect(instance.state).toStrictEqual({ hello: "world" });
+
+    expect(instance.bait.hello).toBe("world");
+    expect(instance.bait.testAction).toBeInstanceOf(Function);
+    expect(instance.bait.testAction()).toBe("works!");
+  });
+});
